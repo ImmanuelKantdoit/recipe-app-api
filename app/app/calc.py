3e1ee00e9d86daa35a1b59b8faee1e68922c6ea6
Miguel Emmanuel Fernandez <m.fernandez.477180@umindanao.edu.ph>
2023-10-25 10:27:20 +0800

    Added psycopg2 and postgresql db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator Functions 
+
+"""
+
+def add(x,y):
+    #Add x and y returns sum 
+    return x + y
+
+def subtract(x,y):
+    #Subtract x and y returns difference 
+    return y-x
+    
